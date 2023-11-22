f66abbdda324a5e8d869911728dc1bb75781c515
Filszu <filipszumowski05@gmail.com>
2023-11-22 17:10:01 +0100

    almost working

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,15 +1,34 @@
 import postLead from '@/lib/dbOperations/postNewLead';
 import { NextResponse, type NextRequest } from 'next/server'
+import { json } from 'stream/consumers';
  
 
 
 export async function POST(request: Request) {
   try{
     
-    const res = await request.json()
+    const requestBody = await request.json()
 
-    console.log('------req.json----->res', res)
-    await postLead(res)
+    console.log('------req.json----->', requestBody)
+
+
+
+
+    // Search Params
+    const { searchParams } = new URL(request.url)
+
+    console.log('------searchParams----->', searchParams)
+    
+    const foo = searchParams.get('foo');
+
+    const offerName = searchParams.get('offer_name');
+
+
+    await postLead(JSON.stringify({
+      searchParams: searchParams,
+      requestBody: requestBody,
+      offerName: offerName,
+    }))
   }catch(e){
     console.log('------req.json----->res', e)
   }
