4e00866ece1fc54e6a43a342f2e3946b8298ae45
Filszu <filipszumowski05@gmail.com>
2023-11-22 16:35:27 +0100

    route update

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,30 +1,19 @@
 import postLead from '@/lib/dbOperations/postNewLead';
 import { NextResponse, type NextRequest } from 'next/server'
  
-export async function POST(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers)
-  const leadData = request.body;
 
 
-    console.log('----------->data', leadData)
-
-    console.log('---==-->requestHeaders', requestHeaders)
-
-
-    // const res = request.formData()
-
-    // console.log('------formadata----->res', res)
-
+export async function POST(request: Request) {
+  try{
+    
     const res = await request.json()
 
     console.log('------req.json----->res', res)
     await postLead(res)
-
-    // return NextResponse.json({ message: 'posted' })
-
-  
-
-  return new NextResponse('Hello, Next.js!', {
+  }catch(e){
+    console.log('------req.json----->res', e)
+  }
+  return new Response('Hello, Next.js!', {
     status: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
@@ -32,13 +21,6 @@ export async function POST(request: NextRequest) {
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
   })
-
-
-
-
-
-
-
 }
 
 export async function GET(request: Request) {
