eae585c30c09270498e7746aedfe1fb6b3ff64df
Filszu <filipszumowski05@gmail.com>
2023-11-22 22:11:44 +0100

    chnaged to get

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -6,6 +6,8 @@ import { NextResponse, type NextRequest } from 'next/server'
 
 
 export async function POST(request: Request) {
+
+  //use form data
   try{
     
     // const requestBody = await request.json()
@@ -83,8 +85,79 @@ export async function POST(request: Request) {
   })
 }
 
+
+// -------------------------------------------
+
 export async function GET(request: Request) {
-  return new Response('Hello :))!', {
+   //use form data
+   try{
+    
+    // const requestBody = await request.json()
+
+    // console.log('------req.json----->', requestBody)
+    // Search Params
+    const { searchParams } = new URL(request.url)
+
+    console.log('------searchParams----->', searchParams??"")
+    
+    // console.log('------searchParamsValues----->', searchParams.values())
+
+    console.log("request.url", request.url)
+
+    const lead: IAdmitadLead = {
+      action: searchParams.get('action') || '',
+      action_id: searchParams.get('action_id') || '',
+      admitad_id: searchParams.get('admitad_id') || '',
+      
+      country_code: searchParams.get('country_code') || '',
+      currency: searchParams.get('currency') || '',
+      offer_id: searchParams.get('offer_id') || '',
+      offer_name: searchParams.get('offer_name') || '',
+     
+     
+      payment_status: searchParams.get('payment_status') || '',
+
+    
+      subid: searchParams.get('subid') || '',
+      subid1: searchParams.get('subid1') || '',
+      subid2: searchParams.get('subid2') || '',
+      subid3: searchParams.get('subid3') || '',
+      subid4: searchParams.get('subid4') || '',
+      type: searchParams.get('type') || '',
+      user_agent: searchParams.get('user_agent') || '',
+      user_referer: searchParams.get('user_referer') || '',
+      website_name: searchParams.get('website_name') || '',
+      // time: searchParams.get('time') || '',
+      // click_time: searchParams.get('click_time') || '',
+      // conversion_time: searchParams.get('conversion_time') || '',
+      // order_id: searchParams.get('order_id') || '',
+      // order_sum: searchParams.get('order_sum') || '',
+      // payment_sum: searchParams.get('payment_sum') || '',
+      // reward_ready: searchParams.get('reward_ready') || '',
+      // website_id: searchParams.get('website_id') || '',
+
+
+
+
+
+  };
+   
+
+
+    await postLead(lead)
+  }catch(e){
+    console.log('------req.json----->res', e)
+    return new Response(`Error ${e}`, {
+      status: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+        })
+
+  }
+  return new Response('Hello, Next.js!', {
     status: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
@@ -93,3 +166,5 @@ export async function GET(request: Request) {
     },
   })
 }
+
+
