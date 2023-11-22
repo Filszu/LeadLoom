44f8328b416e6f0e2b8c7c84f26f08ce3b27a634
Filszu <filipszumowski05@gmail.com>
2023-11-22 18:00:36 +0100

    chcnged idea created new interface

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,3 +1,4 @@
+import { Lead } from '@/database.types';
 import postLead from '@/lib/dbOperations/postNewLead';
 import { NextResponse, type NextRequest } from 'next/server'
 import { json } from 'stream/consumers';
@@ -10,25 +11,49 @@ export async function POST(request: Request) {
     const requestBody = await request.json()
 
     console.log('------req.json----->', requestBody)
-
-
-
-
     // Search Params
     const { searchParams } = new URL(request.url)
 
     console.log('------searchParams----->', searchParams)
     
-    const foo = searchParams.get('foo');
+    const lead: Lead = {
+      action: searchParams.get('action') || null,
+      action_id: searchParams.get('action_id') || null,
+      admitad_id: searchParams.get('admitad_id') || null,
 
-    const offerName = searchParams.get('offer_name');
+      // click_time: parseFloat(searchParams.get('click_time')) || null,
+
+      // click_time: searchParams.get('click_time')!== null ? parseFloat(searchParams.get('click_time')) : null
+
+      conversion_time: parseFloat(searchParams.get('conversion_time')) || null,
+      country_code: searchParams.get('country_code') || null,
+      created_at: new Date().toISOString(), // You may want to use the actual creation time here
+      currency: searchParams.get('currency') || null,
+     
+      info: searchParams.get('info') || null,
+      offer_id: searchParams.get('offer_id') || null,
+      offer_name: searchParams.get('offer_name') || null,
+      order_id: parseFloat(searchParams.get('order_id')) || null,
+      order_sum: parseFloat(searchParams.get('order_sum')) || null,
+      payment_status: searchParams.get('payment_status') || null,
+      payment_sum: parseFloat(searchParams.get('payment_sum')) || null,
+      reward_ready: parseInt(searchParams.get('reward_ready') || '0'), // Replace '0' with the default value if needed
+      subid: searchParams.get('subid') || null,
+      subid1: searchParams.get('subid1') || null,
+      subid2: searchParams.get('subid2') || null,
+      subid3: searchParams.get('subid3') || null,
+      subid4: searchParams.get('subid4') || null,
+      time: parseFloat(searchParams.get('time')) || null,
+      type: searchParams.get('type') || null,
+      user_agent: searchParams.get('user_agent') || null,
+      user_referer: searchParams.get('user_referer') || null,
+      website_id: parseInt(searchParams.get('website_id') || '0'), // Replace '0' with the default website ID if needed
+      website_name: searchParams.get('website_name') || null,
+  };
+   
 
 
-    await postLead(JSON.stringify({
-      searchParams: searchParams,
-      requestBody: requestBody,
-      offerName: offerName,
-    }))
+    await postLead()
   }catch(e){
     console.log('------req.json----->res', e)
   }
@@ -43,7 +68,7 @@ export async function POST(request: Request) {
 }
 
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!', {
+  return new Response('Hello :))!', {
     status: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
---
+++ types.ts
@@ -0,0 +1,29 @@
+interface IAdmitadLead {
+    action: string | null;
+    action_id: string | null;
+    admitad_id: string | null;
+    click_time?: number | null;
+    conversion_time?: number | null;
+    country_code?: string | null;
+    currency?: string | null;
+    offer_id?: string | null;
+    offer_name?: string | null;
+    order_id?: number | null;
+    order_sum?: number | null;
+    payment_status?: string | null;
+    payment_sum?: number | null;
+    reward_ready?: number | null;
+    subid?: string | null;
+    subid1?: string | null;
+    subid2?: string | null;
+    subid3?: string | null;
+    subid4?: string | null;
+    time?: number | null;
+    type?: string | null;
+    user_agent?: string | null;
+    user_referer?: string | null;
+    website_id?: number | null;
+    website_name?: string | null;
+}
+
+
