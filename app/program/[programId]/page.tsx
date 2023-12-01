8ab152ce63ae1035a1d42b3ff9ee2e21ffddd51a
Filszu <filipszumowski05@gmail.com>
2023-12-01 22:14:41 +0100

    updated ref page

--- app/program/[programId]/page.tsx
+++ app/program/[programId]/page.tsx
@@ -5,6 +5,7 @@ import banner from '@/public/imgs/banners/leadloom0.jpg';
 import banner2 from '@/public/imgs/banners/leadLoomBanner.png';
 import mountains from '@/public/imgs/banners/mountains_yellow.jpg';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default async function ProgramPage({
     params,
@@ -27,7 +28,13 @@ export default async function ProgramPage({
 
     if (userName && friendName) {
         msg = `Hey ${friendName}, ${userName} invited you to play🎉. Have fun😉!!`;
+    } else if (userName) {
+        msg = `Hey ${userName}, LeadLoom invited you to play🎉. GLHF🔥!!`;
+    } else if (friendName) {
+        msg = `Hey ${friendName}, your Friend invited you to play🎉. Have fun😉!!`;
     }
+
+    const url = `${program.url}${userName ? `&subid1=${userName}` : ''}${friendName ? `&subid2=${friendName}` : ''}`;
 
     return (
         <section
@@ -50,12 +57,11 @@ export default async function ProgramPage({
                 />
             </div>
             <div
-                className="flex flex-col items-center  justify-center rounded-lg bg-yellow-100 bg-opacity-10 bg-clip-padding p-40 backdrop-blur-md backdrop-filter gap-4
+                className=" flex flex-col  items-center justify-center gap-4 rounded-lg bg-yellow-100 bg-opacity-10 bg-clip-padding p-10 backdrop-blur-md backdrop-filter md:p-20 lg:p-40
 "
             >
-                <h1>{msg}</h1>
-                <h2>{program.programName}</h2>
-                
+                <h1 className="text-3xl">{msg}</h1>
+                <h2 className="text-2xl">{program.programName}</h2>
 
                 <div className="group h-64 w-64 flex-none overflow-clip rounded-md transition-transform">
                     <Image
@@ -69,11 +75,18 @@ export default async function ProgramPage({
                         //     objectFit: 'cover',
                         // }}
 
-                        className="h-full w-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
+                        className="h-full w-full object-cover  transition-transform duration-300 ease-in-out group-hover:scale-105"
                     />
+                    
                 </div>
 
-                <Button className="mr-2 p-6 text-white">PLAY FOR FREE</Button>
+                    {/* <h3>{url}</h3> */}
+
+                <Link href={`${url}`}>
+                    <Button className="mr-2 p-6 text-2xl text-white">
+                        PLAY FOR FREE
+                    </Button>
+                </Link>
             </div>
         </section>
     );
