36690f1d7be9e923ce14592bd759ed71f13c0ef7
Filszu <filipszumowski05@gmail.com>
2023-12-13 17:52:33 +0100

    Refactor page layout and update LeadLoom avatar

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,47 @@
-
-
 import Link from 'next/link';
 // export const dynamic = "force-dynamic"
 import Image from 'next/image';
-import LeadLoomGril from '@/public/imgs/avatars/leadloom_girl.png';
-
+import LeadLoomGirl from '@/public/imgs/avatars/leadloom_girl.png';
 
 import { Button } from '@/components/ui/button';
 import JumpingAvatar from '@/components/landing/components/JumpingAvatar';
 export default async function Home() {
     return (
-        <section className='w-full'>
-        <section className="center flex w-full flex-col  justify-center h-screen">
-            <div className="flex flex-col items-center justify-center">
-                <Image
-                    src={LeadLoomGril}
-                    alt="Picture of the author"
-                    width={500}
-                    height={500}
-                    className=""
-                />
+        <section className="w-full">
+            <section className="center flex h-screen w-full  flex-col items-center justify-center">
+                <div
+                    className="flex w-full flex-col items-center justify-center 
+                "
+                    // md:w-8/12 lg:w-2/12
+                >
+                    <Image
+                        src={LeadLoomGirl}
+                        alt="LeadLoom Girl Avatar"
+                        width={500}
+                        // height={500}
 
-                <div className="bold relative bottom-20 text-center text-8xl font-extrabold tracking-tighter drop-shadow-lg ">
-                    LEADLOOM
-                    <p className="text-2xl font-bold tracking-normal">
-                        Earn amazing rewards by playing games
-                    </p>
+                        // className="w-full"
+                        // style={{
+                        //     objectFit: 'cover', // cover, contain, none
+                        //   }}
+                    />
+
+                    <div className="bold relative bottom-10 text-center text-4xl font-extrabold tracking-tighter drop-shadow-lg md:bottom-20 md:text-8xl ">
+                        LEADLOOM
+                        <p className="text-xl font-bold tracking-normal md:text-2xl">
+                            Earn amazing rewards by playing games
+                        </p>
+                    </div>
+                    <Link href="/dashboard" className="animate-bounce">
+                        <Button className="p-6">
+                            <span className="flex items-center justify-center gap-1 text-lg text-white uppercase">
+                                begin the journey
+                            </span>
+                        </Button>
+                    </Link>
                 </div>
-                <Link href="/dashboard" className="animate-bounce">
-                    <Button className="p-6">
-                        <span className="flex items-center justify-center gap-1 text-lg text-white">
-                            begin the journey
-                        </span>
-                    </Button>
-                </Link>
-            </div>
-
-            
-
-            
-        </section>
-        <JumpingAvatar />
+            </section>
+            <JumpingAvatar />
         </section>
     );
 }
