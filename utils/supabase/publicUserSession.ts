b473b8cb51068e1034bfc75994f090c39faf92a9
Filszu <filipszumowski05@gmail.com>
2023-12-23 19:18:01 +0100

    Fix publicUserSession export

--- utils/supabase/publicUserSession.ts
+++ utils/supabase/publicUserSession.ts
@@ -4,7 +4,7 @@ import getPublicUser from "./getPublicUser";
 // export const publicUserSession = getPublicUser()
 // export const publicUserSession =async ()=>await getPublicUser()
 
-export const publicUserSession = await getPublicUser()
+export const publicUserSession = getPublicUser()
 
 
 // export const publicUserSession= async () => {
