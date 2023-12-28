c329105b050ff9da2e17ea0b825c0b0d8b292993
Filszu <filipszumowski05@gmail.com>
2023-12-28 22:26:45 +0100

    Refactor CardActionsSection component

--- components/program/CardActionsSection.tsx
+++ components/program/CardActionsSection.tsx
@@ -6,48 +6,80 @@ import { FaLocationArrow } from 'react-icons/fa';
 import { useState } from 'react';
 import { Program } from '@/types';
 import { cn } from '@/lib/utils';
-import { PiUserCirclePlusDuotone } from "react-icons/pi";
-import { FaUserPlus } from "react-icons/fa";
-import { PiUserPlusBold } from "react-icons/pi";
+import { PiUserCirclePlusDuotone } from 'react-icons/pi';
+import { FaUserPlus } from 'react-icons/fa';
+import { PiUserPlusBold } from 'react-icons/pi';
 import { useToast } from '../ui/use-toast';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
 
+const CardActionsSection = ({
+    props,
+    nickname,
+}: {
+    props: Program;
+    nickname: string;
+}) => {
+    const { toast } = useToast();
 
-const CardActionsSection = ({props, nickname}:{props:Program, nickname:string}) => {
-
-    const { toast } = useToast()
-    
     const [showProgramInfo, setshowProgramInfo] = useState(false);
     const [acceptedTerms, setAcceptedTerms] = useState(false);
 
-    const programUrl = `/program/${props.programID}?user=${nickname}`
-    const refName ="BRO"
+    const programUrl = `/program/${props.programID}?user=${nickname}`;
+    const refName = 'BRO';
     return (
         <div>
-            <div className='flex gap-2 flex-wrap'>
-                <Button disabled={!acceptedTerms} className="p-6">
-                    <Link href={programUrl} target='_blank'>
-                        <span className="text-white flex justify-center items-center gap-1 text-lg">
-                            GO & PLAY
-                            <FaLocationArrow size={22} />
-                        </span>
-                    </Link>
-                </Button>
-                <Button disabled={!acceptedTerms} className="py-6"
-                onClick={()=>{
-                    toast({
-                        title: `Invite your friends to the game🔥💪`,
-                        description:`You can invite as many friends as you want to the game/challenge👋. Every friend who completes the challenge guarantees you a 100% reward🔥.
-                        🎯But remember that using the same Wi-Fi network or the same IP address will be marked as an invalid prize because it is against the rules. ⭐The default username is ${refName}, you can change this at the top of the address bar`,
-                        variant: "success",
-                      })
-                }}
-                >
-                    <Link href={`${programUrl}&friend=${refName}`} target='_blank'>
-                        <span className="text-white flex justify-center items-center gap-1 text-lg">
+            <div className="flex flex-wrap gap-2">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Button disabled={!acceptedTerms} className="p-6">
+                                <Link href={programUrl} target="_blank">
+                                    <span className="flex items-center justify-center gap-1 text-lg text-white">
+                                        GO & PLAY
+                                        <FaLocationArrow size={22} />
+                                    </span>
+                                </Link>
+                            </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            {!acceptedTerms?(<p>
+                                Before joining the program, please read the
+                                program description and accept the terms and
+                                conditions
+                            </p>):
+                            <p>
+                                🕹️GLHF🫡
+                            </p>
+                            }
                             
-                        {/* <PiUserCirclePlusDuotone size={28} /> */}
-                        <PiUserPlusBold size={28} />
-                        {/* <FaUserPlus size={28} /> */}
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+                <Button
+                    disabled={!acceptedTerms}
+                    className="py-6"
+                    onClick={() => {
+                        toast({
+                            title: `Invite your friends to the game🔥💪`,
+                            description: `You can invite as many friends as you want to the game/challenge👋. Every friend who completes the challenge guarantees you a 100% reward🔥.
+                        🎯But remember that using the same Wi-Fi network or the same IP address will be marked as an invalid prize because it is against the rules. ⭐The default username is ${refName}, you can change this at the top of the address bar`,
+                            variant: 'success',
+                        });
+                    }}
+                >
+                    <Link
+                        href={`${programUrl}&friend=${refName}`}
+                        target="_blank"
+                    >
+                        <span className="flex items-center justify-center gap-1 text-lg text-white">
+                            {/* <PiUserCirclePlusDuotone size={28} /> */}
+                            <PiUserPlusBold size={28} />
+                            {/* <FaUserPlus size={28} /> */}
                         </span>
                     </Link>
                 </Button>
@@ -59,13 +91,14 @@ const CardActionsSection = ({props, nickname}:{props:Program, nickname:string}) 
                         setshowProgramInfo(!showProgramInfo);
                         setAcceptedTerms(true);
                         toast({
-                            title: "Remember to follow the program rules.",
-                            description: "Have fun❣️You can complete each challenge once every 30 days. Don't cheat - any inaccuracies will be detected. And you will be banned🫡",
-                            variant: "success",
-                          })
+                            title: 'Remember to follow the program rules.',
+                            description:
+                                "Have fun❣️You can complete each challenge once every 30 days. Don't cheat - any inaccuracies will be detected. And you will be banned🫡",
+                            variant: 'success',
+                        });
                     }}
                 >
-                    <span className="text-white flex justify-center items-center gap-1 text-lg">
+                    <span className="flex items-center justify-center gap-1 text-lg text-white">
                         READ MORE
                         <FaLocationArrow size={22} />
                     </span>
@@ -86,12 +119,12 @@ const CardActionsSection = ({props, nickname}:{props:Program, nickname:string}) 
 
                     // showProgramInfo && "animate-[accordionDown_1s_ease-in-out]"
 
-                    'grid text-sm overflow-hidden transition-all duration-300 ease-in-out grid-rows-[0fr] opacity-0',
+                    'grid grid-rows-[0fr] overflow-hidden text-sm opacity-0 transition-all duration-300 ease-in-out',
 
                     showProgramInfo && 'grid-rows-[1fr] opacity-100',
                 )}
             >
-                <div className="flex overflow-hidden mt-4 gap-8">
+                <div className="mt-4 flex gap-8 overflow-hidden">
                     {/* Program details */}
                     <section className="flex-1">
                         <h2 className="text-xl font-semibold">
@@ -103,7 +136,7 @@ const CardActionsSection = ({props, nickname}:{props:Program, nickname:string}) 
 
                         {/* Other program details go here */}
                     </section>
-                    <section className='flex-1'>
+                    <section className="flex-1">
                         {/* Example: Pros and Cons */}
                         <div className="mt-4">
                             <h3 className="text-lg font-semibold">Pros</h3>
