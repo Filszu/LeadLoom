719f9e08742056ab8e504fb034bd0e2dbb34c1ee
Filszu <filipszumowski05@gmail.com>
2024-03-11 12:47:24 +0100

    fixed mobile menu

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -10,6 +10,7 @@ import Link from 'next/link';
 import { signOut } from '@/utils/supabase/signOut';
 const Nav = () => {
     const [profileMenuOpen, setprofileMenuOpen] = useState(false);
+    const [mobileMenuOpen, setmobileMenuOpen] = useState(false);
 
     return (
         <nav className="bg-gray-800">
@@ -22,6 +23,7 @@ const Nav = () => {
                             className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                             aria-controls="mobile-menu"
                             aria-expanded="false"
+                            onClick={() => setmobileMenuOpen(!mobileMenuOpen)}
                         >
                             <span className="absolute -inset-0.5"></span>
                             <span className="sr-only">Open main menu</span>
@@ -192,48 +194,50 @@ const Nav = () => {
             </div>
 
             {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-            <div className="sm:hidden" id="mobile-menu">
-                <div className="space-y-1 px-2 pb-3 pt-2">
-                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                    <Link
-                        href="/dashboard"
-                        className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                        aria-current="page"
-                    >
-                        Dashboard
-                    </Link>
-                    <Link
-                        href="/dashboard/reports"
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                        Reports
-                    </Link>
-                    <Link
-                        href="/dashboard/programs"
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                        Programs
-                    </Link>
-                    <Link
-                        href="/dashboard/settings"
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                        Settings
-                    </Link>
-                    <Link
-                        href="/dashboard/affiliate"
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                        Affiliate
-                    </Link>
-                    <Link
-                        href="/dashboard/faq"
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                        FAQ
-                    </Link>
+            {mobileMenuOpen && (
+                <div className="sm:hidden" id="mobile-menu">
+                    <div className="space-y-1 px-2 pb-3 pt-2">
+                        {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                        <Link
+                            href="/dashboard"
+                            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+                            aria-current="page"
+                        >
+                            Dashboard
+                        </Link>
+                        <Link
+                            href="/dashboard/reports"
+                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                        >
+                            Reports
+                        </Link>
+                        <Link
+                            href="/dashboard/programs"
+                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                        >
+                            Programs
+                        </Link>
+                        <Link
+                            href="/dashboard/settings"
+                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                        >
+                            Settings
+                        </Link>
+                        <Link
+                            href="/dashboard/affiliate"
+                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                        >
+                            Affiliate
+                        </Link>
+                        <Link
+                            href="/dashboard/faq"
+                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                        >
+                            FAQ
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            )}
         </nav>
     );
 };
